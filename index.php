5370c42b4b235b61540f4b159e75da7478f898d9
ShaoAnFang <ilovet720419@gmail.com>
2017-01-06 13:32:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ mysql_select_db("sql6152521") or die('Could not select database');
 
 //@mysql_select_db($database) or die("Unable to find database");
 
-$date = date('m/d/Y h:i:s a', time());
+$date = date('Y/m/d_h:i:s a', time());
 
 $name = @$_GET["name"];
 
